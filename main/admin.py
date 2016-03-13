d96e9e00bd906300041509a80ddbccb001cdedb3
linoor <michal.pomaranski@gmail.com>
2016-03-13 11:56:31 +0100

    added models to admin site

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from main.models import Voyage, Place
+
+
+class VoyageAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Voyage, VoyageAdmin)
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Place, PlaceAdmin)
--- main/models.py
+++ main/models.py
@@ -8,6 +8,9 @@ class Place(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(blank=True, max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Voyage(models.Model):
     time_started = models.DateTimeField(blank=False)
@@ -23,4 +26,16 @@ class Voyage(models.Model):
             time_started=self.time_started,
             time_ended=self.time_ended,
             type=self.type
+        )
+
+    def __str__(self):
+        return "id: {id}, time started: {time_started}, " \
+               "time ended: {time_ended}, type: {type}, " \
+               "from place: {from_place}, to place: {to_place}".format(
+            id=self.id,
+            time_started=self.time_started,
+            time_ended=self.time_ended,
+            type=self.type,
+            from_place=self.from_place,
+            to_place=self.to_place
         )

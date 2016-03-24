18ec41227e81df1bc72a609bdcabbf99ba810802
linoor <michal.pomaranski@gmail.com>
2016-03-24 20:02:58 +0100

    setting up webpack

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+
+module.exports = React.createClass({
+   render: function(){
+       return <h1>Hello, world.</h1>
+   }
+})
+
+ReactDOM.render(<App/>, document.getElementById('react-app'))

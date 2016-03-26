7b61b3fc5e40543f1922cec5068713519f2d308a
linoor <michal.pomaranski@gmail.com>
2016-03-26 16:12:42 +0100

    working timer

--- static/js/index.js
+++ static/js/index.js
@@ -15,28 +15,46 @@ var App = React.createClass({
 var Starter = React.createClass({
     getInitialState: function() {
         return {
-            button_text: 'Start'
+            button_text: 'Start',
+            elapsed: 0
         }
     },
 
     componentDidMount: function() {
         this.serverRequest = $.get('/voyage', function (result) {
             if ('time_started' in result) {
+                var time_started = Date.parse(result.time_started);
                 this.setState({
-                    time_started: result.time_started,
-                    button_text: 'Stop',
+                    time_started: time_started,
+                    button_text: 'Stop'
                 })
             }
         }.bind(this));
+
+        this.timer = setInterval(this.tick, 50);
     },
 
-    handleChange: function(e) {
+    componentWillUnmount: function() {
+        clearInterval(this.timer);
+    },
 
+    tick: function() {
+        this.setState({
+            elapsed: Math.abs(new Date() - this.state.time_started)
+        });
     },
 
     render: function() {
+        var delta = this.state.elapsed / 1000;
+
+        var minutes = Math.floor(delta / 60);
+        var seconds = (delta % 60).toFixed(0);
+
         return (
-            <button id="start-button">{this.state.button_text}</button>
+            <div>
+                <p>{minutes}min{seconds}s</p>
+                <button id="start-button">{this.state.button_text}</button>
+            </div>
         )
     }
 });

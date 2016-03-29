dc60c2f142786dae85f6eca0ffb00b9f177eb92f
linoor <michal.pomaranski@gmail.com>
2016-03-29 19:34:00 +0200

    set debug to True

--- timepersistant/settings.py
+++ timepersistant/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i$qhv@i*4rw=euvig2rx5x&q90u175$klv#)hob0xqh#o&ny^)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 if os.environ.get('DEVELOPMENT', None):
     from settings_dev import *
 
